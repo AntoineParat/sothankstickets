981d08d70f006a559d507563fe14a414375109d4
Antoine Parat <paratantoine@gmail.com>
2023-10-21 14:23:24 +0200

    login redirect with spinner

--- src/authContext.js
+++ src/authContext.js
@@ -19,13 +19,15 @@ export function AuthProvider({ children }) {
     const unsubscribe = onAuthStateChanged(auth, user => {
       setCurrentUser(user);
       setLoading(false);
+      console.log("Firebase user:", user);
     });
 
     return unsubscribe;
   }, []);
 
   const value = {
-    currentUser
+    currentUser,
+    loading
   };
 
   return (
@@ -34,3 +36,41 @@ export function AuthProvider({ children }) {
     </AuthContext.Provider>
   );
 }
+
+// authContext.js
+// "use client";
+
+// import { createContext, useContext, useEffect, useState } from 'react';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import { auth } from './firebase';  // Votre configuration firebase
+
+// const AuthContext = createContext();
+
+// export function useAuth() {
+//   return useContext(AuthContext);
+// }
+
+// export function AuthProvider({ children }) {
+//   const [currentUser, setCurrentUser] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, user => {
+//       setCurrentUser(user);
+//       setLoading(false);
+//     });
+
+//     return unsubscribe;
+//   }, []);
+
+//   const value = {
+//     currentUser,
+//     loading // <--- Ajoutez ceci pour rendre l'état de chargement accessible aux consommateurs de ce contexte
+//   };
+
+//   return (
+//     <AuthContext.Provider value={value}>
+//       {!loading && children}
+//     </AuthContext.Provider>
+//   );
+// }
