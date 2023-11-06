2c25b5833d5e056df39245f4f8ce3f3cfe0c015e
Antoine Parat <paratantoine@gmail.com>
2023-11-06 11:47:37 +0100

    fix suggestion gratitudeBox

--- src/components/gratitudeBox.js
+++ src/components/gratitudeBox.js
@@ -233,7 +233,7 @@ export default function GratitudeBox() {
                     setgratitudeDestinataire(e.target.value);
                 }}
                     aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="collaborateur@acadomia.fr" />
-                {gratitudeDestinataire && suggestions.length > 0 && (
+                {suggestions.length > 0 && (
                     <div className="absolute mt-2 rounded border border-gray-300 bg-white z-10">
                         {suggestions.map((suggestion, index) => (
                             <div key={index} onClick={() => {
