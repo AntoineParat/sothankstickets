224ee86e458ce11745d717cc191482d47d8bd9dc
Antoine Parat <paratantoine@gmail.com>
2023-11-06 11:14:51 +0100

    fix useEffect import

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,7 +1,7 @@
 "use client"
 
-import { useState } from "react";
-import { useEffect, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from 'next/navigation';
 import Link from "next/link";
 
 import { auth } from '../../firebase';
